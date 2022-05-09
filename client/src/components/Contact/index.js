db95487d79f985dc7a93e5abaee977a0beac9125
robyng <robyngraham01@gmail.com>
2022-05-09 12:56:05 -0700

    trying to catch error on addMessage

--- client/src/components/Contact/index.js
+++ client/src/components/Contact/index.js
@@ -9,78 +9,74 @@ function Contact() {
     const [messageText, setText] = useState('');
     const [characterCount, setCharacterCount] = useState(0);
 
-    
-    const [addMessage, { error }] = useMutation(ADD_MESSAGE);
-    // , {
-    //     update(cache, { data: { addMessage } }) {
-    //       try {
-    //         // update thought array's cache
-    //         // could potentially not exist yet, so wrap in a try/catch
-    //         const { messages } = cache.readQuery({ query: QUERY_MESSAGES });
-    //         cache.writeQuery({
-    //           query: QUERY_MESSAGES,
-    //           data: { messages: [addMessage, ...messages] },
-    //         });
-    //       } catch (e) {
-    //         console.error(e);
-    //       }
-    
-          // update me object's cache
-        //   const { me } = cache.readQuery({ query: QUERY_ME });
-        //   cache.writeQuery({
-        //     query: QUERY_ME,
-        //     data: { me: { ...me, messages: [...me.messages, addMessage] } },
-        //   });
-    //     },
+
+    const [addMessage, { error }] = useMutation(ADD_MESSAGE, {
+        update(cache, { data: { addMessage } }) {
+          try {
+            // update thought array's cache
+            // could potentially not exist yet, so wrap in a try/catch
+            const { messages } = cache.readQuery({ query: QUERY_MESSAGES });
+            
+               // prepend the newest thought to the front of the array
+            cache.writeQuery({
+              query: QUERY_MESSAGES,
+              data: { messages: [addMessage, ...messages] },
+            });
+          } catch (e) {
+            console.error(">>>>>>>> catching error ", e, " | ", error);
+          }
+
+    // update me object's cache
+    //   const { me } = cache.readQuery({ query: QUERY_ME });
+    //   cache.writeQuery({
+    //     query: QUERY_ME,
+    //     data: { me: { ...me, messages: [...me.messages, addMessage] } },
     //   });
+        },
+      });
 
-        // update state based on form input changes
-  const handleChange = (event) => {
-    if (event.target.value.length <= 20) {
-      setText(event.target.value);
-      setCharacterCount(event.target.value.length);
+    // update state based on form input changes
+    const handleChange = (event) => {
+        if (event.target.value.length <= 20) {
+            setText(event.target.value);
+            setCharacterCount(event.target.value.length);
 
-    }
-  };
+        }
+    };
 
     const handleSubmit = async event => {
         // prevent page refresh
         event.preventDefault();
-addMessage(messageText);
-console.log(error)
+        // addMessage(messageText);
+        // console.log(error)
         // clear form value
         setText('');
         setCharacterCount(0);
-    
-        // try {
-        //   await addMessage({
-        //     variables: { messageText },
-        //   });
-    
 
-         
-  
-        // } catch (e) {
-        //   console.error(e);
-        // }
-      };
+        try {
+          await addMessage();
 
-    return(
+        } catch (e) {
+          console.error(e);
+        }
+    };
+
+    return (
         <section>
             <p className={`m-0 ${characterCount === 20 || error ? 'text-error' : ''}`}>
-  Character Count: {characterCount}/20
-  {error && <span className="ml-2"> Something went wrong...</span>}
-</p>
+                Character Count: {characterCount}/20
+                {error && <span className="ml-2"> Something went wrong...</span>}
+            </p>
             <form onSubmit={handleSubmit}>
-            <textarea
-          placeholder="Here's a new thought..."
-          value={messageText}
-          className="form-input col-12 col-md-9"
-          onChange={handleChange}
-        ></textarea>
-        <button className="btn btn-success" type="submit">
-          Submit
-        </button>
+                <textarea
+                    placeholder="Here's a new thought..."
+                    value={messageText}
+                    className="form-input col-12 col-md-9"
+                    onChange={handleChange}
+                ></textarea>
+                <button className="btn btn-success" type="submit">
+                    Submit
+                </button>
             </form>
 
         </section>
@@ -92,8 +88,8 @@ console.log(error)
 
 //     const [formState, setFormState] = useState('');
 //     const { username, email, messageText } = formState;
-  
-   
+
+
 //     const [addMessage, { error }] = useMutation(ADD_MESSAGE, {
 //         update(cache, { data: { addMessage } }) {
 //           try {
@@ -107,7 +103,7 @@ console.log(error)
 //           } catch (e) {
 //             console.error(e);
 //           }
-    
+
 //           // update me object's cache
 //           const { me } = cache.readQuery({ query: QUERY_MESSAGE });
 //           cache.writeQuery({
@@ -167,7 +163,7 @@ console.log(error)
 
 //     return (
 //         <section className="container form-div col-lg-6 col-md-10">
-        
+
 //             <h2>Contact me</h2>
 //             <a href='mailto:robyn@sitereworks.com'  target='_blank' rel='noreferrer'>robyn@sitereworks.com</a>
 //             <br />
